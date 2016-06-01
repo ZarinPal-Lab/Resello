0c3f8cd0ee077acf9f1dbbe8ea1df1f350ba9d0e
Mostafa Amiri <amiri27@gmail.com>
2016-06-01 07:04:14 -0400

    Applied fixes from StyleCI

--- conf.php
+++ conf.php
@@ -1,27 +1,24 @@
 <?php
 /**
- * @package    Resello custom payment gateway
  * @author     Ahmad Rajabi (Ahmad@rajabi.us)
  * @copyright  2016 Ahmad Rajabi
  */
-$merchantCode = ""; // set merchant code here . for example = "123456" (string)
-$secret_key   = ""; //set secret key 1 (string)
-$secret_key2  = ""; // set secret key 2 (string)
-$notify       = ""; // set notify address (string)
-$url          = "http://"; //set domain customer panel link (string)
-$backurl      = "http://"; //set getresult.php path (string)
-$rset         = 38; // change currency from usd to toman (integer)
+$merchantCode = ''; // set merchant code here . for example = "123456" (string)
+$secret_key = ''; //set secret key 1 (string)
+$secret_key2 = ''; // set secret key 2 (string)
+$notify = ''; // set notify address (string)
+$url = 'http://'; //set domain customer panel link (string)
+$backurl = 'http://'; //set getresult.php path (string)
+$rset = 38; // change currency from usd to toman (integer)
 define('USER', ''); //set database username (string)
 define('PASS', ''); //set database password (string)
 define('DBHOST', 'localhost'); //set database address (string)
 define('DBNAME', 'yourdbname'); //set database name (string)
-try{
-$pdo = "mysql:host=".DBHOST.";dbname=".DBNAME;
-$db = new PDO($pdo,USER,PASS);
-return $db ;
-}
-catch(PDOException $e){
+try {
+    $pdo = 'mysql:host='.DBHOST.';dbname='.DBNAME;
+    $db = new PDO($pdo, USER, PASS);
 
-	echo "Database connection failed";
+    return $db;
+} catch (PDOException $e) {
+    echo 'Database connection failed';
 }
-?>
